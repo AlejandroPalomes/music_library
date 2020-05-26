a83e5e06dc42cc35d6322fba6cb6dad317ca2418
Alex Palomes <alejandropalomes@hotmail.com>
2020-05-26 19:15:08 +0200

    artist almost complete

--- assets/js/artist.js
+++ assets/js/artist.js
@@ -1,14 +1,13 @@
 var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-
-
 function createArtist(artist){
 // function createArtist(artistJSON){
     // artist = JSON.parse(artistJSON)
-    //console.log(artist.amgArtistId);
+    console.log(artist);
+    console.log("createArtist called")
     $("#main__container").append(`
-    <div id ="${artist.amgArtistId}" class="main__container__artist">
-        <h3>${artist.artistName}</h3>
+    <div id ="${artist.amgArtistId}" class="main__container__artist d-flex flex-column">
+        <a href="${artist.artistLinkUrl}" class="main__container__artist--title mb-3"><h3>${artist.artistName}</h3></a>
         <div class="main__container__artist--songs d-flex flex-wrap justify-content-between">
         </div>
     </div>
@@ -17,10 +16,24 @@ function createArtist(artist){
         url: `https://itunes.apple.com/lookup?amgArtistId=${artist.amgArtistId}&entity=song&limit=5&sort=recent`,
         success: (result)=> {
             var data = (JSON.parse(result).results)
-            $(data).each((i,e)=>{
-                var n = new Date(e.releaseDate);
-                if(i != 0) $(`#${artist.amgArtistId} .main__container__artist--songs`).append(createSong(e, month, n))
-            })
+            $(`#${artist.amgArtistId} .main__container__artist--songs`).empty()
+            if(data.length){
+                $(data).each((i,e)=>{
+                    console.log("****called each****");
+                    var n = new Date(e.releaseDate);
+                    if(i != 0) $(`#${artist.amgArtistId} .main__container__artist--songs`).append(createSong(e, month, n));
+                })
+            }else{
+                $(`#${artist.amgArtistId}`).remove();
+            }
+        },
+        complete: ()=> {
+            $(".result").hover((e) => {
+                // console.log($(`#h${e.currentTarget.id}`))
+                $(`#h${e.currentTarget.id}`).toggleClass("d-none");
+            });
+
+            heartClik();
         }
     });
     
