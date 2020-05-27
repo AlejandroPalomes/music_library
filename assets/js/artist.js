c01d600afbdd219acdafe475bb6611a83086f918
Alex Palomes <alejandropalomes@hotmail.com>
2020-05-27 14:28:30 +0200

    es5 on comments

--- assets/js/artist.js
+++ assets/js/artist.js
@@ -53,3 +53,55 @@ function createArtist(artist){
     });
     
 }
+
+// "use strict";
+
+// var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+// function createArtist(artist) {
+//   // function createArtist(artistJSON){
+//   // artist = JSON.parse(artistJSON)
+//   // console.log(artist);
+//   // console.log("createArtist called")
+//   $("#main__container").append("\n    <div id =\"" + artist.amgArtistId + "\" class=\"main__container__artist d-flex flex-column\">\n        <a href=\"" + artist.artistLinkUrl + "\" class=\"main__container__artist--title mb-4 d-flex flex-column\">\n        <h3>" + artist.artistName + "</h3>\n        <h5 class=\"mb-2\">" + artist.primaryGenreName + "</h5>\n        </a>\n        <div class=\"main__container__artist--songs d-flex flex-wrap justify-content-between\">\n        </div>\n    </div>\n    ");
+//   $.ajax({
+//     url: "https://itunes.apple.com/lookup?amgArtistId=" + artist.amgArtistId + "&entity=song&limit=5&sort=recent",
+//     success: function success(result) {
+//       var data = JSON.parse(result).results;
+//       $("#" + artist.amgArtistId + " .main__container__artist--songs").empty();
+
+//       if (data.length) {
+//         $(data).each(function (i, e) {
+//           // console.log("****called each****");
+//           var n = new Date(e.releaseDate);
+//           if (i != 0) $("#" + artist.amgArtistId + " .main__container__artist--songs").append(createSong(e, month, n));
+//         });
+//       } else {
+//         $("#" + artist.amgArtistId).remove();
+//       }
+//     },
+//     complete: function complete() {
+//       $(".result").hover(function (e) {
+//         // console.log($(`#h${e.currentTarget.id}`))
+//         $("#h" + e.currentTarget.id).toggleClass("d-none");
+//       });
+//       $(".main__target__preview__btn").click(function (e) {
+//         $("#audioPlayer").prop("src", $(e.currentTarget).data("preview"));
+
+//         if ($("#audioPlayer").data("paused") == true) {
+//           document.querySelector("#audioPlayer").play();
+//           $("#audioPlayer").data("paused", false);
+//         } else {
+//           document.querySelector("#audioPlayer").pause();
+//           $("#audioPlayer").data("paused", true);
+//         }
+
+//         ;
+//         $('#toggleControls').click();
+//         return false;
+//       });
+//       heartFill();
+//       heartClik();
+//     }
+//   });
+// }
--- assets/js/script.js
+++ assets/js/script.js
@@ -40,7 +40,10 @@ function favLoad() {
                         $("#audioPlayer").data("paused", true);
                     };
                     $('#toggleControls').click();
-                    $('#toggleControls').text();
+                    $('#display__title').text($(`#p${e.trackId}`).data("title"));
+                    $('#display__album').text($(`#p${e.trackId}`).data("album"));
+                    $('#display__artist').text($(`#p${e.trackId}`).data("artist"));
+                    $('#display__cover').prop("src", $(`#p${e.trackId}`).data("cover"));
 
                     return false;
 
@@ -269,3 +272,271 @@ function removeSong(storage, element) {
 
     localStorage.setItem("favMusic", JSON.stringify(storage))
 }
+
+// "use strict";
+
+// var iTunesURI = "https://itunes.apple.com/search?";
+// var endpoint = "term=babymetal&limit=50&country=jp";
+// var countryValue;
+// var tempLocalStorage;
+// var objFav = {
+//   trackId: 0,
+//   type: ""
+// };
+// var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+// $.ajax({
+//   url: "https://www.liferay.com/api/jsonws/country/get-countries/",
+//   success: function success(data) {
+//     $(data).each(function (i, country) {
+//       $("#countrySelect").append("<option value=\"" + country.a2 + "\" name=\"test\">" + country.nameCurrentValue + "</option>");
+//     });
+//   }
+// });
+
+// function favLoad() {
+//   var fav = getLocalStorage();
+//   $("#main__container").empty();
+//   $(fav).each(function (i, e) {
+//     $.ajax({
+//       url: "https://itunes.apple.com/lookup?id=" + e.trackId,
+//       dataType: "jsonp",
+//       success: function success(track) {
+//         printResults(track.results[0], e.type, true);
+//       },
+//       complete: function complete() {
+//         $("#p" + e.trackId).click(function (a) {
+//           $("#audioPlayer").prop("src", $("#p" + e.trackId).data("preview"));
+
+//           if ($("#audioPlayer").data("paused") == true) {
+//             document.querySelector("#audioPlayer").play();
+//             $("#audioPlayer").data("paused", false);
+//           } else {
+//             document.querySelector("#audioPlayer").pause();
+//             $("#audioPlayer").data("paused", true);
+//           }
+
+//           ;
+//           $('#toggleControls').click();
+//           $('#toggleControls').text();
+//           return false;
+//         });
+//         $("#" + e.trackId).hover(function (a) {
+//           $("#h" + a.currentTarget.id).toggleClass("d-none");
+//         });
+//         $("#h" + e.trackId).addClass("fillHeart");
+//         $("#h" + e.trackId + " .st0").addClass("st0-2");
+//         $("#h" + e.trackId).click(function (a) {
+//           $(a.currentTarget).toggleClass("fillHeart");
+//           $("#" + a.currentTarget.id + " .st0").toggleClass("st0-2");
+//           objFav.trackId = e.trackId;
+//           objFav.type = e.type;
+//           getLocalStorage();
+//           saveLocalSorage(objFav); // favLoad();
+
+//           return false;
+//         });
+//       }
+//     });
+//   });
+// }
+
+// favLoad();
+// $("#inputArtist").keyup(updateSearch);
+// $("#explicit").change(updateSearch);
+// $("#limitSelect").change(updateSearch);
+// $("#countrySelect").change(updateSearch);
+// $("#fieldSelect").change(updateSearch);
+
+// function updateSearch() {
+//   endpoint = "term=" + $("#inputArtist").prop("value");
+
+//   if ($("#fieldSelect").val() != null) {
+//     endpoint += "&entity=" + $("#fieldSelect option:selected").val();
+//   }
+
+//   if ($("#countrySelect option:selected").val() != null) {
+//     endpoint += "&country=" + $("#countrySelect option:selected").val();
+//   }
+
+//   if ($("#limitSelect option:selected").val() != null) {
+//     endpoint += "&limit=" + $("#limitSelect option:selected").val();
+//   }
+
+//   getResults(iTunesURI, endpoint);
+// } // )
+
+
+// function getResults(iTunesURI, endpoint) {
+//   $.ajax({
+//     url: iTunesURI,
+//     data: endpoint + "&lang=ja_jp",
+//     dataType: "jsonp",
+//     success: function success(result) {
+//       //logResults(result.results)
+//       $("#main__container").empty();
+//       printResults(result.results);
+//     },
+//     error: function error() {
+//       return console.log("fuck");
+//     },
+//     complete: function complete() {
+//       $(".result").hover(function (e) {
+//         $("#h" + e.currentTarget.id).toggleClass("d-none");
+//       });
+//       heartFill();
+//       $(".main__target__preview__btn").click(function (e) {
+//         $("#audioPlayer").prop("src", $(e.currentTarget).data("preview"));
+
+//         if ($("#audioPlayer").data("paused") == true) {
+//           document.querySelector("#audioPlayer").play();
+//           $("#audioPlayer").data("paused", false);
+//         } else {
+//           document.querySelector("#audioPlayer").pause();
+//           $("#audioPlayer").data("paused", true);
+//         }
+
+//         ;
+//         $('#toggleControls').click();
+//         return false;
+//       });
+//       heartClik();
+//     }
+//   });
+// }
+
+// function printResults(result, type, fav) {
+//   var contentType;
+//   type ? contentType = type : contentType = $("#fieldSelect").val();
+
+//   switch (contentType) {
+//     case "song":
+//       $(result).each(function (i, e) {
+//         var n = new Date(e.releaseDate);
+
+//         if ($("#explicit").prop("checked") || fav) {
+//           $("#main__container").append(createSong(e, month, n));
+//         } else {
+//           if (e.collectionExplicitness == "notExplicit") {
+//             $("#main__container").append(createSong(e, month, n));
+//           }
+//         }
+//       });
+//       break;
+
+//     case "album":
+//     case "Album":
+//       $(result).each(function (i, e) {
+//         var n = new Date(e.releaseDate);
+
+//         if ($("#explicit").prop("checked") || fav) {
+//           $("#main__container").append(createAlbum(e, month, n));
+//         } else {
+//           if (e.contentAdvisoryRating != "Explicit" || e.collectionExplicitness == "notExplicit") {
+//             $("#main__container").append(createAlbum(e, month, n));
+//           }
+//         }
+//       });
+//       break;
+
+//     case "allArtist":
+//       $(result).each(function (i, e) {
+//         if (e.wrapperType == "artist") {
+//           createArtist(e);
+//         }
+//       });
+//       break;
+
+//     default:
+//       $(result).each(function (i, e) {
+//         // if ($("#explicit").prop("checked")) {
+//         var n = new Date(e.releaseDate);
+//         $("#main__container").append(createSong(e, month, n)); // $("#main__container").append(createSong(e, month, n));
+//         // }
+//       });
+//   }
+// }
+
+// function logResults(result) {
+//   console.log(result);
+// }
+
+// function heartClik() {
+//   $(".heart").click(function (e) {
+//     // console.log($(e.currentTarget).parent().parent().prop("id"))
+//     var trackIdCurrent = $(e.currentTarget).parent().parent().prop("id");
+//     var currentType = $(e.currentTarget).parent().parent().data("type");
+//     objFav.trackId = trackIdCurrent;
+//     objFav.type = currentType;
+//     $(e.currentTarget).toggleClass("fillHeart");
+//     $("#" + e.currentTarget.id + " .st0").toggleClass("st0-2");
+//     getLocalStorage();
+//     saveLocalSorage(objFav);
+//     return false;
+//   });
+//   heartFill();
+// }
+
+// function heartFill() {
+//   var storage = getLocalStorage();
+//   $(".result").each(function (i, song) {
+//     $(storage).each(function (i, fav) {
+//       if (song.id == fav.trackId) {
+//         console.log("i'm in fucking fav bitch");
+//         $("#h" + song.id).addClass("fillHeart");
+//         $("#h" + song.id + " .st0").addClass("st0-2");
+//       }
+//     });
+//   });
+// }
+
+// function checkStorage(song) {
+//   var trackIdCurrent = $(song).parent().parent().prop("id");
+//   var currentType = $(song).parent().parent().data("type");
+//   objFav.trackId = trackIdCurrent;
+//   objFav.type = currentType;
+//   getLocalStorage();
+//   saveLocalSorage(objFav);
+//   return false;
+// }
+
+// function getLocalStorage() {
+//   tempLocalStorage = JSON.parse(localStorage.getItem("favMusic"));
+//   return tempLocalStorage;
+// }
+
+// function saveLocalSorage(obj1) {
+//   var storage = getLocalStorage();
+
+//   if (storage) {
+//     var include = storage.filter(function (fav) {
+//       return fav.trackId == obj1.trackId;
+//     });
+//     include.length ? removeSong(storage, obj1) : uploadStorage(storage, obj1);
+//   } else {
+//     var arr = [];
+//     arr.push(obj1);
+//     localStorage.setItem("favMusic", JSON.stringify(arr));
+//     return;
+//   }
+// }
+
+// function uploadStorage(storage, obj1) {
+//   storage.push(obj1);
+//   localStorage.setItem("favMusic", JSON.stringify(storage)); // return false;
+// }
+
+// function removeSong(storage, element) {
+//   var index;
+//   $(storage).each(function (i, song) {
+//     if (song.trackId == element.trackId) {
+//       index = i;
+//     }
+//   });
+
+//   if (index > -1) {
+//     storage.splice(index, 1);
+//   }
+
+//   ;
+//   localStorage.setItem("favMusic", JSON.stringify(storage));
+// }
